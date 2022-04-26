3b0acc72c57ad0793f6d4048fccd20944dc74ff7
MichelleLan90 <michelle.lanaro90@gmail.com>
2022-04-26 21:00:02 +0200

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Navbar() {
   return (
@@ -7,7 +9,16 @@ function Navbar() {
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                    LOGO
+                  <h3><FontAwesomeIcon icon={solid('headset')} style={{color: 'white', fontSize: '28px'}}/></h3>
+                </Link>
+                <Link to="/aboutme" className="link-text">
+                  <h3>about me</h3>
+                </Link>
+                <Link to="/projects" className="link-text">
+                  <h3>projects</h3>
+                </Link>
+                <Link to="/contact" className="link-text">
+                  <h3>contact</h3>
                 </Link>
                 <div className='menu-icon'>
                     
